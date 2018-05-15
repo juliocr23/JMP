d2b74d23e0eabce86244179cef3f30d3d2fe1f58
Julio Rosario <rosariojulio40@gmail.com>
2018-05-15 12:30:49 -0400

    Add files via upload

--- src/Entity/SubBoss.java
+++ src/Entity/SubBoss.java
@@ -24,7 +24,12 @@ public class SubBoss extends Rectangle{
 	
 	public SubBoss(double x, double y,int total, String file) {
 		super(x,y,0,0);
-		
+
+		totalHits = total;
+		numberOfHits = 0;
+
+		isDead = false;
+
 		explosion = new Animation("Resources/explosion/explosion_", ".png", 38, 10);
 		missile1 = new ArrayList<>();
 		
@@ -37,39 +42,44 @@ public class SubBoss extends Rectangle{
 		}
 	}
 	
-	public void update(double px,double py) {
+	public void update(double px,double pw) {
 		
-		shootTimer += 0.1;
+		shootTimer += 0.01;
 	
+
+			if (px - pw < x)
+				x--;
+			else
+				x++;
+
 		
-       /* if (px-pw < x) 
-           x--;
-        else 
-           x++;*/
-		
-		updateDirection(px,py);
-        
-        if(numberOfHits == totalHits)
-        	isDead = true;
-        
-        if(shootTimer >= 1) {
-        	missile1.add(new Missile(x+25,y+60,false));
-			shootTimer = 0;
+		//updateDirection(px,py);
+
+		System.out.println("Number of hits:  " + numberOfHits);
+        if(numberOfHits >= totalHits) {
+			isDead = true;
+			System.out.println("Is dead");
 		}
         
+        if(shootTimer >= 1) {
+        	missile1.add(new Missile(x+38,y+60,false));
+			shootTimer = 0;
+		}
+
         for(int i = 0; i< missile1.size(); i++)
 			missile1.get(i).launch();
 	}
 	
 	public void draw(Graphics g) {
 		
-		//if(isDead) {
-		//	g.drawImage(explosion.nextImage(), (int)x, (int)y, null);
-		//}else {
+		if(isDead && !explosion.isAnimationOver()) {
+			g.drawImage(explosion.nextImage(), (int)x, (int)y, null);
+		}
+		else if(!isDead) {
 		    g.drawImage(img, (int)x,(int)y,width,height,null);
 		    for(int i = 0; i< missile1.size(); i++)
 				missile1.get(i).draw(g);
-		//}
+		}
 	}
 	
 	private void updateDirection(double x2, double y2){
@@ -80,5 +90,21 @@ public class SubBoss extends Rectangle{
 
 		x += 2 * Math.cos( angle );
 		y += 2 * Math.sin( angle );
+	}
+
+	public ArrayList<Missile> getMissile1() {
+		return missile1;
+	}
+
+	public void addHit(){
+		numberOfHits++;
+	}
+
+	public boolean isDead(){
+		return  isDead && explosion.isAnimationOver();
+	}
+
+	public boolean isExploading(){
+		return isDead;
 	}
 }
